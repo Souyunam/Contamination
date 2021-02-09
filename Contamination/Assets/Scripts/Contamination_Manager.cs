0d684d9149eae1bae87a791f1a2c71ee8fb89420
Nathan <nat.rig@live.fr>
2021-02-09 16:47:07 +0100

    Contamination OK

--- Contamination/Assets/Scripts/Contamination_Manager.cs
+++ Contamination/Assets/Scripts/Contamination_Manager.cs
@@ -6,13 +6,29 @@ namespace Contamination
 {
     public class Contamination_Manager : MonoBehaviour
     {
+        #region Variables
         public static Contamination_Manager instance;
 
-        [SerializeField] public GameObject prefabPanda;
-        public List<Panda> forest;
+        [SerializeField] private GameObject prefabPanda;
 
+        private List<Panda> forest;
+        public List<Panda> contagiousPandas;
+        #endregion
 
-        private void Awake()
+        #region ProtectionVariable
+        public void AddContagiousPanda(Panda newContagousPanda)
+        {
+            contagiousPandas.Add(newContagousPanda);
+        }
+
+        public void RemoveContagiousPanda(Panda curedPanda)
+        {
+            contagiousPandas.Remove(curedPanda);
+        }
+        #endregion
+
+        #region Instance
+        private void Awake() 
         {
             // We create an instance of this script
             if (instance == null)
@@ -20,9 +36,34 @@ namespace Contamination
                 instance = this;
             }
         }
+        #endregion
+
+        private void Start()
+        {
+            forest = new List<Panda>();
+            contagiousPandas = new List<Panda>();
+
+            CreateForest(3);
+            //Set patientZero
+            forest[0].IsContaminated();
+            //Spread the disease
+            //Create curves
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Debug.Log("appuie espace");
+                spreadingDisease(contagiousPandas);
+            }
+        }
 
         private void CreateForest(int forestDimension)
-            ///This will create a forest as a cube forestDimension x forestDimension x forestDimension///
+            ///<summary>
+            ///This will create a forest as a cube forestDimension x forestDimension x forestDimension
+            ///</summary>
+            ///<param name="forestDimension"> number of pandas into the forest (dimension of the cube)</param>
         {
             for (int i = 0; i < forestDimension; i++)
             {
@@ -43,10 +84,26 @@ namespace Contamination
             }
         }
 
-        private void Start()
-        {
-            CreateForest(3);
-        }
 
+
+        //Selection 
+
+
+        //Contamination
+        private void spreadingDisease(List<Panda> listPandas)
+            ///<summary>
+            ///For panda who are still contagious continue to spread the disease in the forest
+            ///</summary>
+            ///<param name="listStillContagiousPanda"> this will contains the list of pandas who can still contaminate other panda</param>
+        {
+            Panda[] currentContagiousPandas = new Panda[listPandas.Count];
+            listPandas.CopyTo(currentContagiousPandas);
+
+            foreach (Panda panda in currentContagiousPandas)
+            {
+                print(listPandas.Count);
+                panda.Contaminate();
+            }
+        }
     }
 }
--- Contamination/Assets/Scripts/Panda.cs
+++ Contamination/Assets/Scripts/Panda.cs
@@ -6,24 +6,60 @@ namespace Contamination
 {
     public class Panda : MonoBehaviour
     {
+        #region Variables
         [SerializeField] private Color colorNormal;
         [SerializeField] private Color colorContaminated;
 
-        private bool isContaminated; /* true : have been contaminated */
-        private bool isContagious; /*this should could help to set an infection rate*/
-        [SerializeField] private List<Panda> neighboors; /*this will contains all the Neighboors that should be contaminated*/
+        [SerializeField] private bool isContaminated = false; /* true : have been contaminated */
+        [SerializeField] private bool isContagious = false; /*this should could help to set an infection rate*/
 
-        private void Start()
+        [SerializeField] private List<Panda> neighboors; /*this will contains all the Neighboors that should be contaminated*/
+        #endregion
+
+        public void IsContaminated()
         {
-            isContagious = false;
-            isContaminated = false;
-            neighboors = new List<Panda>();
+            ///<summary> this will changes attributes when this panda is contaminated as color or if he is contagious</summary>
+            isContagious = true;
+            isContaminated = true;
+            this.gameObject.GetComponent<Renderer>().material.color = colorContaminated;
+            Contamination_Manager.instance.AddContagiousPanda(this);
         }
 
 
+        public void cure()
+        {
+            ///<summary> this will change attributes when a panda is cure in this way he/she can't be contaminated again and will no longueur spread the virus</summary>
+            isContagious = false;
+            Contamination_Manager.instance.RemoveContagiousPanda(this);
+        }
+        public void Contaminate()
+        {
+            ///<summary> if the current panda is contagious this spread the disease to its neighboors</summary>
+            if(isContagious)
+            {
+                foreach (Panda neighboor in this.neighboors)
+                {
+                    if(!neighboor.isContaminated)
+                    {
+                        neighboor.IsContaminated();
+                    }
+
+                }
+                return;
+            }
+
+            else
+            {
+                return;
+            }
+        }
+
 
         public void FindNeighboors(List<Panda> forest)
-        /// this will find all the neighboors of of current panda amoung a list in parameter calls forest ////
+            ///<summary>
+            /// this will find all the neighboors of of current panda amoung a list in parameter calls forest ////
+            ///</summary>
+            ///<param name="forest"> list of pandas into the forest</param>
         {
             
             List<Vector3> listPositionPossible = new List<Vector3>();
